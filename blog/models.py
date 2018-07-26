2ab2cec7b97e65d66d25966b9afb3cacf0dcbf04
Matthew <mbdevaney@gmail.com>
2018-07-25 20:54:03 -0500

    added a comments feature

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -10,6 +25,9 @@ class Post(models.Model):
         default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
     def publish(self):
         self.published_date = timezone.now()
