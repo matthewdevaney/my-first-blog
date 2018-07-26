1eeea22f46d9cba1be34b2e2be523101dfdfb6e8
Matthew <mbdevaney@gmail.com>
2018-07-26 12:18:27 -0500

    misc

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views
-from django.urls import include, path
+from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
